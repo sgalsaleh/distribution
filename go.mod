27bc653ab85618320022178b68ca3bc799d113e4
Salah Al Saleh <sg.alsaleh@gmail.com>
2024-02-27 15:42:39 +0000

    don't bail if an error is encountered during garbage collection

--- go.mod
+++ go.mod
@@ -88,7 +88,7 @@ require (
 	go.opentelemetry.io/otel/metric v1.21.0 // indirect
 	go.opentelemetry.io/otel/sdk/metric v1.21.0 // indirect
 	go.opentelemetry.io/proto/otlp v1.0.0 // indirect
-	golang.org/x/sync v0.7.0
+	golang.org/x/sync v0.8.0
 	golang.org/x/sys v0.21.0 // indirect
 	golang.org/x/text v0.16.0 // indirect
 	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
--- registry/storage/garbagecollect.go
+++ registry/storage/garbagecollect.go
@@ -2,13 +2,14 @@ package storage
 
 import (
 	"context"
-	"errors"
 	"fmt"
+	"sync"
 
 	"github.com/distribution/distribution/v3"
 	"github.com/distribution/distribution/v3/registry/storage/driver"
 	"github.com/distribution/reference"
 	"github.com/opencontainers/go-digest"
+	"golang.org/x/sync/errgroup"
 )
 
 func emit(format string, a ...interface{}) {
@@ -35,100 +36,149 @@ func MarkAndSweep(ctx context.Context, storageDriver driver.StorageDriver, regis
 		return fmt.Errorf("unable to convert Namespace to RepositoryEnumerator")
 	}
 
+	// enumerate in parallel
+	const concurrencyLimit = 100
+	var markSetMtx sync.Mutex
+	var deleteLayerSetMtx sync.Mutex
+	var manifestArrMtx sync.Mutex
+	repoG, _ := errgroup.WithContext(context.Background())
+	repoG.SetLimit(concurrencyLimit)
+	manifestG, _ := errgroup.WithContext(context.Background())
+	manifestG.SetLimit(concurrencyLimit)
+
 	// mark
 	markSet := make(map[digest.Digest]struct{})
 	deleteLayerSet := make(map[string][]digest.Digest)
 	manifestArr := make([]ManifestDel, 0)
 	err := repositoryEnumerator.Enumerate(ctx, func(repoName string) error {
-		emit(repoName)
+		func(repoName string) {
+			repoG.Go(func() error {
+				emit(repoName)
 
-		var err error
-		named, err := reference.WithName(repoName)
-		if err != nil {
-			return fmt.Errorf("failed to parse repo name %s: %v", repoName, err)
-		}
-		repository, err := registry.Repository(ctx, named)
-		if err != nil {
-			return fmt.Errorf("failed to construct repository: %v", err)
-		}
-
-		manifestService, err := repository.Manifests(ctx)
-		if err != nil {
-			return fmt.Errorf("failed to construct manifest service: %v", err)
-		}
-
-		manifestEnumerator, ok := manifestService.(distribution.ManifestEnumerator)
-		if !ok {
-			return fmt.Errorf("unable to convert ManifestService into ManifestEnumerator")
-		}
-
-		err = manifestEnumerator.Enumerate(ctx, func(dgst digest.Digest) error {
-			if opts.RemoveUntagged {
-				// fetch all tags where this manifest is the latest one
-				tags, err := repository.Tags(ctx).Lookup(ctx, distribution.Descriptor{Digest: dgst})
+				var err error
+				named, err := reference.WithName(repoName)
 				if err != nil {
-					return fmt.Errorf("failed to retrieve tags for digest %v: %v", dgst, err)
-				}
-				if len(tags) == 0 {
-					// fetch all tags from repository
-					// all of these tags could contain manifest in history
-					// which means that we need check (and delete) those references when deleting manifest
-					allTags, err := repository.Tags(ctx).All(ctx)
-					if err != nil {
-						if _, ok := err.(distribution.ErrRepositoryUnknown); ok {
-							emit("manifest tags path of repository %s does not exist", repoName)
-							return nil
-						}
-						return fmt.Errorf("failed to retrieve tags %v", err)
-					}
-					manifestArr = append(manifestArr, ManifestDel{Name: repoName, Digest: dgst, Tags: allTags})
+					emit("failed to parse repo name %s: %v", repoName, err)
 					return nil
 				}
-			}
-			// Mark the manifest's blob
-			emit("%s: marking manifest %s ", repoName, dgst)
-			markSet[dgst] = struct{}{}
-
-			return markManifestReferences(dgst, manifestService, ctx, func(d digest.Digest) bool {
-				_, marked := markSet[d]
-				if !marked {
-					markSet[d] = struct{}{}
-					emit("%s: marking blob %s", repoName, d)
+				repository, err := registry.Repository(ctx, named)
+				if err != nil {
+					emit("failed to construct repository: %v", err)
+					return nil
 				}
-				return marked
+
+				manifestService, err := repository.Manifests(ctx)
+				if err != nil {
+					emit("failed to construct manifest service: %v", err)
+					return nil
+				}
+
+				manifestEnumerator, ok := manifestService.(distribution.ManifestEnumerator)
+				if !ok {
+					emit("unable to convert ManifestService into ManifestEnumerator")
+					return nil
+				}
+
+				err = manifestEnumerator.Enumerate(ctx, func(dgst digest.Digest) error {
+					func(ctx context.Context, dgst digest.Digest) {
+						manifestG.Go(func() error {
+							if opts.RemoveUntagged {
+								// fetch all tags where this manifest is the latest one
+								tags, err := repository.Tags(ctx).Lookup(ctx, distribution.Descriptor{Digest: dgst})
+								if err != nil {
+									emit("failed to retrieve tags for digest %v: %v", dgst, err)
+									return nil
+								}
+								if len(tags) == 0 {
+									// fetch all tags from repository
+									// all of these tags could contain manifest in history
+									// which means that we need check (and delete) those references when deleting manifest
+									allTags, err := repository.Tags(ctx).All(ctx)
+									if err != nil {
+										if _, ok := err.(distribution.ErrRepositoryUnknown); ok {
+											emit("manifest tags path of repository %s does not exist", repoName)
+											return nil
+										}
+										emit("failed to retrieve tags %v", err)
+										return nil
+									}
+									manifestArrMtx.Lock()
+									manifestArr = append(manifestArr, ManifestDel{Name: repoName, Digest: dgst, Tags: allTags})
+									manifestArrMtx.Unlock()
+									return nil
+								}
+							}
+							// Mark the manifest's blob
+							emit("%s: marking manifest %s ", repoName, dgst)
+							markSetMtx.Lock()
+							markSet[dgst] = struct{}{}
+							markSetMtx.Unlock()
+
+							return markManifestReferences(dgst, manifestService, ctx, func(d digest.Digest) bool {
+								markSetMtx.Lock()
+								defer markSetMtx.Unlock()
+								_, marked := markSet[d]
+								if !marked {
+									markSet[d] = struct{}{}
+									emit("%s: marking blob %s", repoName, d)
+								}
+								return marked
+							})
+						})
+					}(ctx, dgst)
+
+					return nil
+				})
+
+				if err != nil {
+					// In certain situations such as unfinished uploads, deleting all
+					// tags in S3 or removing the _manifests folder manually, this
+					// error may be of type PathNotFound.
+					//
+					// In these cases we can continue marking other manifests safely.
+					if _, ok := err.(driver.PathNotFoundError); !ok {
+						emit("path not found: %v", err)
+						return nil
+					}
+				}
+				blobService := repository.Blobs(ctx)
+				layerEnumerator, ok := blobService.(distribution.ManifestEnumerator)
+				if !ok {
+					emit("unable to convert BlobService into ManifestEnumerator")
+					return nil
+				}
+
+				var deleteLayers []digest.Digest
+				err = layerEnumerator.Enumerate(ctx, func(dgst digest.Digest) error {
+					markSetMtx.Lock()
+					defer markSetMtx.Unlock()
+					if _, ok := markSet[dgst]; !ok {
+						deleteLayers = append(deleteLayers, dgst)
+					}
+					return nil
+				})
+				if len(deleteLayers) > 0 {
+					deleteLayerSetMtx.Lock()
+					deleteLayerSet[repoName] = deleteLayers
+					deleteLayerSetMtx.Unlock()
+				}
+				if err != nil {
+					emit("error enumerating layers: %v", err)
+				}
+				return nil
 			})
-		})
+		}(repoName)
 
-		if err != nil {
-			// In certain situations such as unfinished uploads, deleting all
-			// tags in S3 or removing the _manifests folder manually, this
-			// error may be of type PathNotFound.
-			//
-			// In these cases we can continue marking other manifests safely.
-			if _, ok := err.(driver.PathNotFoundError); !ok {
-				return err
-			}
-		}
-		blobService := repository.Blobs(ctx)
-		layerEnumerator, ok := blobService.(distribution.ManifestEnumerator)
-		if !ok {
-			return errors.New("unable to convert BlobService into ManifestEnumerator")
-		}
-
-		var deleteLayers []digest.Digest
-		err = layerEnumerator.Enumerate(ctx, func(dgst digest.Digest) error {
-			if _, ok := markSet[dgst]; !ok {
-				deleteLayers = append(deleteLayers, dgst)
-			}
-			return nil
-		})
-		if len(deleteLayers) > 0 {
-			deleteLayerSet[repoName] = deleteLayers
-		}
-		return err
+		return nil
 	})
 	if err != nil {
-		return fmt.Errorf("failed to mark: %v", err)
+		emit("failed to mark: %v", err)
+	}
+	if err := repoG.Wait(); err != nil {
+		emit("error enumerating repositories: %v", err)
+	}
+	if err := manifestG.Wait(); err != nil {
+		emit("error enumerating manifests: %v", err)
 	}
 
 	manifestArr = unmarkReferencedManifest(manifestArr, markSet)
@@ -139,7 +189,7 @@ func MarkAndSweep(ctx context.Context, storageDriver driver.StorageDriver, regis
 		for _, obj := range manifestArr {
 			err = vacuum.RemoveManifest(obj.Name, obj.Digest, obj.Tags)
 			if err != nil {
-				return fmt.Errorf("failed to delete manifest %s: %v", obj.Digest, err)
+				emit("failed to delete manifest %s: %v", obj.Digest, err)
 			}
 		}
 	}
@@ -153,7 +203,7 @@ func MarkAndSweep(ctx context.Context, storageDriver driver.StorageDriver, regis
 		return nil
 	})
 	if err != nil {
-		return fmt.Errorf("error enumerating blobs: %v", err)
+		emit("error enumerating blobs: %v", err)
 	}
 	emit("\n%d blobs marked, %d blobs and %d manifests eligible for deletion", len(markSet), len(deleteSet), len(manifestArr))
 	for dgst := range deleteSet {
@@ -163,7 +213,7 @@ func MarkAndSweep(ctx context.Context, storageDriver driver.StorageDriver, regis
 		}
 		err = vacuum.RemoveBlob(string(dgst))
 		if err != nil {
-			return fmt.Errorf("failed to delete blob %s: %v", dgst, err)
+			emit("failed to delete blob %s: %v", dgst, err)
 		}
 	}
 
@@ -175,7 +225,7 @@ func MarkAndSweep(ctx context.Context, storageDriver driver.StorageDriver, regis
 			}
 			err = vacuum.RemoveLayer(repo, dgst)
 			if err != nil {
-				return fmt.Errorf("failed to delete layer link %s of repo %s: %v", dgst, repo, err)
+				emit("failed to delete layer link %s of repo %s: %v", dgst, repo, err)
 			}
 		}
 	}
